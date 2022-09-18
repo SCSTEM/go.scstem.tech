67beb214dc0b76a484933cb7ea633f38683dbb5f
CS-5 <me@carsonseese.com>
2022-09-18 18:45:03 -0400

    Base functionality

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,34 @@
-import { Hono } from "hono";
-import { api } from "./api";
-
 export interface Env {
   KV_LINKS: KVNamespace;
+  DEFAULT_URL: string;
 }
 
-const app = new Hono();
+export default {
+  async fetch(req: Request, env: Env): Promise<Response> {
+    try {
+      const code = new URL(req.url).pathname.replace("/", "");
+      const url = await getRedirect(env.KV_LINKS, code);
 
-app.route("/api", api);
+      return Response.redirect(url.toString());
+    } catch (_) {
+      return Response.redirect(env.DEFAULT_URL);
+    }
+  },
+};
 
-export default app;
+const getRedirect = async (kv: KVNamespace, code: string): Promise<URL> => {
+  try {
+    // Get the link from the KV store
+    const link = await kv.get(code);
+    if (link === null) return Promise.reject();
+
+    // Attempt to convert it to a URL, if it can't be converted,
+    // assume that code is reserved and reject
+    const url = new URL(link);
+
+    return Promise.resolve(url);
+  } catch (_) {
+    // No need to error, just ignore the request
+    return Promise.reject();
+  }
+};
